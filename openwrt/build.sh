523f0474b76aace7c031020411e94dc1c9c3ffd8
fattiger <nobody0036@proton.me>
2024-05-16 13:40:08 +0800

    passssr

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -230,7 +230,7 @@ EOF
 [ "$(whoami)" = "runner" ] && endgroup
 
 # 删除重复的软件包
-rm -rf ../openwrt/feeds/immortal/applications/{luci-app-accesscontrol,luci-app-airplay2,luci-app-alist,luci-app-argon-config,luci-app-autoreboot,luci-app-cpufreq,luci-app-daed,luci-app-diskman,luci-app-eqos,luci-app-filebrowser,luci-app-mentohust,luci-app-netdata,luci-app-passwall,luci-app-qbittorrent,luci-app-ramfree,luci-app-socat,luci-app-unblockneteasemusic,luci-app-usb-printer,luci-app-vlmcsd,luci-app-zerotier}
+rm -rf ../openwrt/feeds/immortal/applications/{luci-app-shadowsocks-libev,luci-app-accesscontrol,luci-app-airplay2,luci-app-alist,luci-app-argon-config,luci-app-autoreboot,luci-app-cpufreq,luci-app-daed,luci-app-diskman,luci-app-eqos,luci-app-filebrowser,luci-app-mentohust,luci-app-netdata,luci-app-passwall,luci-app-qbittorrent,luci-app-ramfree,luci-app-socat,luci-app-unblockneteasemusic,luci-app-usb-printer,luci-app-vlmcsd,luci-app-zerotier}
 rm -rf ../openwrt/feeds/immortal/themes/luci-theme-argon
 
 # 安装 feeds
