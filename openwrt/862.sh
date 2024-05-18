a7d635d7105a8a8c59d33eb9c5dc3f37fa342acf
fattiger <nobody0036@proton.me>
2024-05-18 17:22:02 +0800

    86123

--- openwrt/862.sh
+++ openwrt/862.sh
@@ -1,3 +1,60 @@
+#!/bin/bash -e
+export RED_COLOR='\e[1;31m'
+export GREEN_COLOR='\e[1;32m'
+export YELLOW_COLOR='\e[1;33m'
+export BLUE_COLOR='\e[1;34m'
+export PINK_COLOR='\e[1;35m'
+export SHAN='\e[1;33;5m'
+export RES='\e[0m'
+export mirror=raw.githubusercontent.com/gitbruc/cooluc-op/master
+export gitea=git.cooluc.com
+export github="github.com"
+
+# Source branch
+if [ "$1" = "dev" ]; then
+    export branch=openwrt-23.05
+    export version=snapshots-23.05
+    export toolchain_version=openwrt-23.05
+elif [ "$1" = "rc2" ]; then
+    latest_release="v$(curl -s https://$mirror/tags/v23)"
+    export branch=$latest_release
+    export version=rc2
+    export toolchain_version=openwrt-23.05
+fi
+
+# platform
+[ "$2" = "x86_64" ] && export platform="x86_64" toolchain_arch="x86_64"
+
+# print version
+echo -e "\r\n${GREEN_COLOR}Building $branch${RES}\r\n"
+if [ "$platform" = "x86_64" ]; then
+    echo -e "${GREEN_COLOR}Model: x86_64${RES}"
+fi
+
+# gcc13 & 14 & 15
+if [ "$USE_GCC13" = y ]; then
+    export USE_GCC13=y
+    # use mold
+    [ "$USE_MOLD" = y ] && USE_MOLD=y
+elif [ "$USE_GCC14" = y ]; then
+    export USE_GCC14=y
+    # use mold
+    [ "$USE_MOLD" = y ] && USE_MOLD=y
+elif [ "$USE_GCC15" = y ]; then
+    export USE_GCC15=y
+    # use mold
+    [ "$USE_MOLD" = y ] && USE_MOLD=y
+fi
+
+# use glibc
+export USE_GLIBC=$USE_GLIBC
+
+# lrng
+export ENABLE_LRNG=$ENABLE_LRNG
+
+# kernel build with clang lto
+export KERNEL_CLANG_LTO=$KERNEL_CLANG_LTO
+
 # 安装 feeds
 [ "$(whoami)" = "runner" ] && group "feeds install -a"
 ./scripts/feeds install -a
@@ -7,6 +64,27 @@
 if [ -f ../dl.gz ]; then
     tar xf ../dl.gz -C .
 fi
+
+
+if [ "$USE_GCC13" = "y" ]; then
+    echo -e "${GREEN_COLOR}GCC VERSION: 13${RES}"
+elif [ "$USE_GCC14" = "y" ]; then
+    echo -e "${GREEN_COLOR}GCC VERSION: 14${RES}"
+elif [ "$USE_GCC15" = "y" ]; then
+    echo -e "${GREEN_COLOR}GCC VERSION: 15${RES}"
+else
+    echo -e "${GREEN_COLOR}GCC VERSION: 11${RES}"
+fi
+[ -n "$LAN" ] && echo -e "${GREEN_COLOR}LAN: $LAN${RES}" || echo -e "${GREEN_COLOR}LAN: 10.0.0.1${RES}"
+[ "$USE_MOLD" = "y" ] && echo -e "${GREEN_COLOR}USE_MOLD: true${RES}" || echo -e "${GREEN_COLOR}USE_MOLD: false${RES}"
+[ "$ENABLE_OTA" = "y" ] && echo -e "${GREEN_COLOR}ENABLE_OTA: true${RES}" || echo -e "${GREEN_COLOR}ENABLE_OTA: false${RES}"
+[ "$ENABLE_BPF" = "y" ] && echo -e "${GREEN_COLOR}ENABLE_BPF: true${RES}" || echo -e "${GREEN_COLOR}ENABLE_BPF: false${RES}"
+[ "$ENABLE_LTO" = "y" ] && echo -e "${GREEN_COLOR}ENABLE_LTO: true${RES}" || echo -e "${GREEN_COLOR}ENABLE_LTO: false${RES}"
+[ "$ENABLE_LRNG" = "y" ] && echo -e "${GREEN_COLOR}ENABLE_LRNG: true${RES}" || echo -e "${GREEN_COLOR}ENABLE_LRNG: false${RES}"
+[ "$BUILD_FAST" = "y" ] && echo -e "${GREEN_COLOR}BUILD_FAST: true${RES}" || echo -e "${GREEN_COLOR}BUILD_FAST: false${RES}"
+[ "$MINIMAL_BUILD" = "y" ] && echo -e "${GREEN_COLOR}MINIMAL_BUILD: true${RES}" || echo -e "${GREEN_COLOR}MINIMAL_BUILD: false${RES}"
+[ "$KERNEL_CLANG_LTO" = "y" ] && echo -e "${GREEN_COLOR}KERNEL_CLANG_LTO: true${RES}\r\n" || echo -e "${GREEN_COLOR}KERNEL_CLANG_LTO: false${RES}\r\n"
+
 
 ###############################################
 echo -e "\n${GREEN_COLOR}Patching ...${RES}\n"
